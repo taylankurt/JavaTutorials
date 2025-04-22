149df322543b1e2811658022844ee46eea2b696d
Taylan Kurt <taylan.kurt@dataphone.at>
2025-04-22 15:40:26 +0200

    project1 finished until mortgage calculation

--- Part1/HelloWorld/src/Main.java
+++ Part1/HelloWorld/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: " );
+        int principal = scanner.nextInt();
 
-        System.out.println("Hello, World Maaaan");
+        System.out.print("Annual Interest Rate: " );
+        float annualRate = scanner.nextFloat();
+        float monthlyInterestRate = (annualRate /100) /12;
+
+        System.out.print("Period (Years): " );
+        int period = scanner.nextInt();
+        int numberOfPayments = period * 12;
+
+        double mortgage = principal
+                * (monthlyInterestRate * Math.pow (1+ monthlyInterestRate, numberOfPayments))
+                / (Math.pow(1+ monthlyInterestRate, numberOfPayments) -1) ;
+        System.out.println(mortgage);
+
     }
 }
---
+++ Part1/HelloWorld/src/Project1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Project1 {
+    Scanner scanner = new Scanner(System.in);
+    int principal = scanner.nextInt();
+    float interestRate = scanner.nextFloat();
+    int period = scanner.nextInt();
+
+}
